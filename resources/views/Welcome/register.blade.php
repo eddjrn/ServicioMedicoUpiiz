50043af1f8f7a94d3466a4a02e37c219513d3801
BelmontIzacc <jisagiizacc@gmail.com>
2017-05-23 16:34:33 -0500

    0.00022

--- resources/views/Welcome/register.blade.php
+++ resources/views/Welcome/register.blade.php
@@ -8,87 +8,23 @@
 @stop
 
 @section('subHead')
-Servicio Medico Upiiz4
+Registro de Nuevo Alumno
 @stop
 
 @section('content')
 <div class="box-typical box-typical-padding documentation">
     <header class="documentation-header">
-        <h2>Objetivo</h2>
+        <h5>Formulario</h5>
         <p class="lead color-blue-grey"> </p>
     </header>
     <div class="text-block text-block-typical">
         <p>
-            Tenemos como objetivo principal el que los alumnos de nuevo ingreso no batallen con el llendo de la documentación médica.
+           
         </p>
-        <p>mediante instructivos  y para los demás alumnos contar con su expediente completo.
-        Izaccccc</p>
+        <p> </p>
         
     </div>
 </div><!--.box-typical-->
-
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-5">
-            <section class="widget">
-                <header class="widget-header-dark">Mapas Imss</header>
-                
-
-                <div class="tab-content widget-tabs-content">
-
-                    <div class="tab-pane" id="w-1-tab-0" role="tabpanel" aria-expanded="false">
-                        <center><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d41623.14776222582!2d-102.59717057394697!3d22.765334713626213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824931098d672b%3A0x81e350358c34bbd9!2sIMSS+Subdelegaci%C3%B3n+Zacatecas%2C+Av.+Solidaridad+2%2C+Militar%2C+98617+Guadalupe%2C+Zac.!3m2!1d22.7581779!2d-102.5222299!5e0!3m2!1ses!2smx!4v1495584465921" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></center>
-                    </div>
-
-                    <div class="tab-pane" id="w-1-tab-1" role="tabpanel" aria-expanded="false">
-                        <center><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d12373.766879175771!2d-102.58526506211027!3d22.77492056162897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824e93068b2cb3%3A0xe9af7064ac9c92b7!2sIMSS%2C+Avenida+Universidad+102%2C+Lomas+del+Patrocinio%2C+98060+Zacatecas%2C+Zac.!3m2!1d22.7662625!2d-102.5598576!5e0!3m2!1ses!2smx!4v1495545674418" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></center>
-                    </div>
-                    <div class="tab-pane" id="w-1-tab-2" role="tabpanel" aria-expanded="false">
-                        <center><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d58863.39533191359!2d-102.61268627254599!3d22.76678373115269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824ec8e848a215%3A0x37337628f866ac2e!2sAv.+M%C3%A9xico%2C+Guadalupe%2C+Zac.!3m2!1d22.7608093!2d-102.5396004!5e0!3m2!1ses!2smx!4v1495583519603" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></center>
-                    </div>
-                    <div class="tab-pane active" id="w-1-tab-3" role="tabpanel" aria-expanded="true">
-                        <center><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d58866.371709448445!2d-102.59695057258196!3d22.759879582056154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUnidad+Profesional+Interdisciplinaria+de+Ingenier%C3%ADa+Campus+Zacatecas%2C+Cerro+del+Gato+Ejido+la+Escondida%2C+Col.+Ciudad+Administrativa%2C+Boulevard+El+Bote+S%2FN%2C+98160+Zacatecas%2C+Zac.!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x14075c6919446cab%3A0x24ebe537d6f1b527!2sIMSS%2C+Calle+1910+123%2C+Centro%2C+98600+Guadalupe%2C+Zac.!3m2!1d22.7469552!2d-102.5122234!5e0!3m2!1ses!2smx!4v1495568557449" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></center>
-                    </div>
-
-                </div>
-                
-                <div class="widget-tabs-nav bordered">
-                    <ul class="tbl-row" role="tablist">
-
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#w-1-tab-0" role="tab" aria-expanded="false">
-                                <i class="font-icon font-icon-pin"></i>
-                                Sub Delegacion
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#w-1-tab-1" role="tab" aria-expanded="false">
-                                <i class="font-icon font-icon-pin"></i>
-                                Clinica 1
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#w-1-tab-2" role="tab" aria-expanded="false">
-                                <i class="font-icon font-icon-pin"></i>
-                                Clinica 4
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" data-toggle="tab" href="#w-1-tab-3" role="tab" aria-expanded="true">
-                                <i class="font-icon font-icon-pin"></i>
-                                Clinica 3
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </section><!--.widget-->
-        </div>
-        <div class="col-md-3"></div>
-    </div>
-</div>
 @stop
 
 @section('scripts')
