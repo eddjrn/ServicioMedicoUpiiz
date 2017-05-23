971d6052d92b7516200aa71546b966f37f86bc94
eddjrn <eddjrn@gmail.com>
2017-05-23 16:46:08 -0500

    0.00023

--- resources/views/Welcome/register.blade.php
+++ resources/views/Welcome/register.blade.php
@@ -12,21 +12,36 @@ Registro de Nuevo Alumno
 @stop
 
 @section('content')
-<div class="box-typical box-typical-padding documentation">
-    <header class="documentation-header">
-        <h5>Formulario</h5>
-        <p class="lead color-blue-grey"> </p>
-    </header>
-    <div class="text-block text-block-typical">
-        <p>
-           
-        </p>
-        <p> </p>
-        
+<div class="page-center">
+    <div class="container-fluid">
+        <form class="sign-box">
+            <div class="sign-avatar no-photo">&plus;</div>
+            <header class="sign-title">UPIIZ</header>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="E-Mail"/>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Boleta"/>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Repetir boleta"/>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Contraseña"/>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Repetir Contraseña"/>
+            </div>
+            <button type="submit" class="btn btn-rounded btn-success sign-up">Registrar</button>
+            <p class="sign-note">¿Tienes una cuenta? <a href="sign-in.html">Iniciar sesion</a></p>
+            <!--<button type="button" class="close">
+                <span aria-hidden="true">&times;</span>
+            </button>-->
+        </form>
     </div>
-</div><!--.box-typical-->
+</div><!--.page-center-->
 @stop
 
 @section('scripts')
-    <script src="Template/js/lib/select2/select2.full.min.js"></script>
+    
 @stop
