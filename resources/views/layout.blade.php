1c297449f9776ee78f3291655f847c6edfc859a5
eddjrn <eddjrn@gmail.com>
2017-05-23 12:37:53 -0500

    0.0009

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,7 @@
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	
     <link rel="stylesheet" href="Template/css/lib/font-awesome/font-awesome.min.css">
     <link rel="stylesheet" href="Template/css/main.css">
 </head>
@@ -30,159 +31,42 @@
     <header class="site-header">
 	    <div class="container-fluid">
 	        <a href="#" class="site-logo">
-	            <img class="hidden-md-down" src="Template/img/logo-2.png" alt="">
-	            <img class="hidden-lg-up" src="Template/img/logo-2-mob.png" alt="">
-	        </a>
+	            <img class="hidden-md-down" src="Template/img/logo upiiz estilo ipn_small2.png" alt="">
+	            <img class="hidden-lg-up" src="Template/img/logo upiiz estilo ipn_small3.png" alt="">
+	        </a>       
+	        
 	        <button class="hamburger hamburger--htla">
 	            <span>toggle menu</span>
 	        </button>
+	        
 	        <div class="site-header-content">
+	        
 	            <div class="site-header-content-in">
 	                <div class="site-header-shown">
-	                    
-	
-	                    <div class="dropdown dropdown-notification messages">
-	                        <a href="#"
-	                           class="header-alarm dropdown-toggle active"
-	                           id="dd-messages"
-	                           data-toggle="dropdown"
-	                           aria-haspopup="true"
-	                           aria-expanded="false">
-	                            <i class="font-icon-mail"></i>
-	                        </a>
-	                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-messages" aria-labelledby="dd-messages">
-	                            <div class="dropdown-menu-messages-header">
-	                                <ul class="nav" role="tablist">
-	                                    <li class="nav-item">
-	                                        <a class="nav-link active"
-	                                           data-toggle="tab"
-	                                           href="#tab-incoming"
-	                                           role="tab">
-	                                            Inbox
-	                                            <span class="label label-pill label-danger">8</span>
-	                                        </a>
-	                                    </li>
-	                                    <li class="nav-item">
-	                                        <a class="nav-link"
-	                                           data-toggle="tab"
-	                                           href="#tab-outgoing"
-	                                           role="tab">Outbox</a>
-	                                    </li>
-	                                </ul>
-	                                <!--<button type="button" class="create">
-	                                    <i class="font-icon font-icon-pen-square"></i>
-	                                </button>-->
-	                            </div>
-	                            
-	                            <div class="tab-content">
-	                                <div class="tab-pane active" id="tab-incoming" role="tabpanel">
-	                                    <div class="dropdown-menu-messages-list">
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/photo-64-2.jpg" alt=""></span>
-	                                            <span class="mess-item-name">Tim Collins</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something!</span>
-	                                        </a>
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/avatar-2-64.png" alt=""></span>
-	                                            <span class="mess-item-name">Christian Burton</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something! Morgan was bothering about something.</span>
-	                                        </a>
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/photo-64-2.jpg" alt=""></span>
-	                                            <span class="mess-item-name">Tim Collins</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something!</span>
-	                                        </a>
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/avatar-2-64.png" alt=""></span>
-	                                            <span class="mess-item-name">Christian Burton</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something...</span>
-	                                        </a>
-	                                    </div>
-	                                </div>
-	                                
-	                                <div class="tab-pane" id="tab-outgoing" role="tabpanel">
-	                                    <div class="dropdown-menu-messages-list">
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/avatar-2-64.png" alt=""></span>
-	                                            <span class="mess-item-name">Christian Burton</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something! Morgan was bothering about something...</span>
-	                                        </a>
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/photo-64-2.jpg" alt=""></span>
-	                                            <span class="mess-item-name">Tim Collins</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something! Morgan was bothering about something.</span>
-	                                        </a>
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/avatar-2-64.png" alt=""></span>
-	                                            <span class="mess-item-name">Christian Burtons</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something!</span>
-	                                        </a>
-	                                        <a href="#" class="mess-item">
-	                                            <span class="avatar-preview avatar-preview-32"><img src="Template/img/photo-64-2.jpg" alt=""></span>
-	                                            <span class="mess-item-name">Tim Collins</span>
-	                                            <span class="mess-item-txt">Morgan was bothering about something!</span>
-	                                        </a>
-	                                    </div>
-	                                </div>
-	                            </div>
-	                            <div class="dropdown-menu-notif-more">
-	                                <a href="#">See more</a>
-	                            </div>
-	                        </div>
-	                    </div>
-	
+                        
 	                    <div class="dropdown user-menu">
 	                        <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	                            <img src="Template/img/avatar-2-64.png" alt="">
 	                        </button>
 	                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-user"></span>Profile</a>
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-cog"></span>Settings</a>
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-question-sign"></span>Help</a>
+	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-user"></span>Perfil</a>
+	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-cog"></span>Configuración</a>
 	                            <div class="dropdown-divider"></div>
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-log-out"></span>Logout</a>
+	                            <a class="dropdown-item" href="#"><span class="font-icon font-icon-share"></span>Facebook</a>
+	                            <a class="dropdown-item" href="#"><span class="font-icon font-icon-home"></span>Sitio UPIIZ</a>
+	                            <div class="dropdown-divider"></div>
+	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-log-out"></span>Salir</a>
 	                        </div>
 	                    </div>
-	
-	                    <button type="button" class="burger-right">
-	                        <i class="font-icon-menu-addl"></i>
-	                    </button>
+	                    
+	                    <a href="#" class="site-logo">
+                            <img class="hidden-md-down" src="Template/img/escudo ipn negro_small.png" alt="">
+                        </a>
+	                    
 	                </div><!--.site-header-shown-->
-	
-	                <div class="mobile-menu-right-overlay"></div>
 	                
-	                <div class="site-header-collapsed">
-	                    <div class="site-header-collapsed-in">
-	                        <div class="dropdown dropdown-typical">
-	                            <div class="dropdown-menu" aria-labelledby="dd-header-sales">
-	                                <a class="dropdown-item" href="#"><span class="font-icon font-icon-home"></span>Quant and Verbal</a>
-	                            </div>
-	                        </div>
-	                        
-	                        <div class="dropdown dropdown-typical">
-	                            <a class="dropdown-toggle" id="dd-header-marketing" data-target="#" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	                                <span class="font-icon font-icon-cogwheel"></span>
-	                                <span class="lbl">Marketing automation</span>
-	                            </a>
-	
-	                            <div class="dropdown-menu" aria-labelledby="dd-header-marketing">
-	                                <a class="dropdown-item" href="#">Current Search</a>
-	                                <div class="dropdown-more">
-	                                    <div class="dropdown-more-caption padding">more...</div>
-	                                    <div class="dropdown-more-sub">
-	                                        <div class="dropdown-more-sub-in">
-	                                            <a class="dropdown-item" href="#"><span class="font-icon font-icon-home"></span>Quant and Verbal</a>
-	                                        </div>
-	                                    </div>
-	                                </div>
-	                                
-	                                <div class="dropdown-divider"></div>
-	                                <a class="dropdown-item" href="#">Import Issues from CSV</a>
-	                            </div>
-	                            
-	                        </div>
-                        </div>
-                    </div>
+	                <span class="lbl hidden-md-down" style="font-size:25px">Unidad Profesional Interdisciplinaria de Ingeniería Campus Zacatecas</span>
+                    <span class="lbl hidden-lg-up" style="font-size:25px">UPIIZ-IPN</span>
                     
 	            </div><!--site-header-content-in-->
 	        </div><!--.site-header-content-->
@@ -195,36 +79,14 @@
 			<a class="nav-link" href="#">Contact</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" href="#">Prperties</a>
-		</li>
-		<li class="nav-item">
 			<a class="nav-link active" href="#">Documents</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Sales</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Projects</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Contact</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Prperties</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Documents</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Sales</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Projects</a>
 		</li>
 	</ul>
 	
 	<div class="page-content">
 		<div class="container-fluid">
+            <h3 class="with-border">@yield('subHead')</h3>
+            
             @yield('content')
 		</div><!--.container-fluid-->
 	</div><!--.page-content-->
