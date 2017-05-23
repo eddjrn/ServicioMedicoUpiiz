7116be8bf0f37f3453ac6cfcc9fe287f13b87992
BelmontIzacc <jisagiizacc@gmail.com>
2017-05-23 08:53:42 -0500

    0.0008

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,42 @@
 @stop
 
 @section('content')
+<div class="container-fluid">
+			<div class="box-typical box-typical-padding documentation">
+				<footer class="documentation-meta">
+					<h2 class="text-center" >Servicio Medico Upiiz</h2>
+				</footer>
 
+				<header class="documentation-header">
+					<h2>Objetivo</h2>
+					<p class="lead color-blue-grey"> </p>
+				</header>
+				<div class="text-block text-block-typical">
+					<p>
+						Tenemos como objetivo principal el que los alumnos de nuevo ingreso no batallen con el llendo de la documentación médica.
+					</p>
+					<p>mediante instructivos  y para los demás alumnos contar con su expediente completo.</p>
+					<p>
+						
+					</p>
+					<h4>Mapas</h4>
+				
+						<div class="container-fluid">
+							<div class="row">
+							<h5>Upiiz - Clinica 1</h5>
+							<div class="col-md-4"><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d12373.766879175771!2d-102.58526506211027!3d22.77492056162897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824e93068b2cb3%3A0xe9af7064ac9c92b7!2sIMSS%2C+Avenida+Universidad+102%2C+Lomas+del+Patrocinio%2C+98060+Zacatecas%2C+Zac.!3m2!1d22.7662625!2d-102.5598576!5e0!3m2!1ses!2smx!4v1495545674418" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+							<div class="col-md-4"><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d12373.766879175771!2d-102.58526506211027!3d22.77492056162897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824e93068b2cb3%3A0xe9af7064ac9c92b7!2sIMSS%2C+Avenida+Universidad+102%2C+Lomas+del+Patrocinio%2C+98060+Zacatecas%2C+Zac.!3m2!1d22.7662625!2d-102.5598576!5e0!3m2!1ses!2smx!4v1495545674418" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+  							<div class="col-md-4"><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d12373.766879175771!2d-102.58526506211027!3d22.77492056162897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824e93068b2cb3%3A0xe9af7064ac9c92b7!2sIMSS%2C+Avenida+Universidad+102%2C+Lomas+del+Patrocinio%2C+98060+Zacatecas%2C+Zac.!3m2!1d22.7662625!2d-102.5598576!5e0!3m2!1ses!2smx!4v1495545674418" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+						</div>
+						</div>
+						
+			
+					<p>
+						
+					</p>
+				</div>
+			</div><!--.box-typical-->
+		</div>
 @stop
 
 @section('scripts')
