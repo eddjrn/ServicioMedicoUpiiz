43b2c21ab349c29f942703bfecef601a4c1af1dc
BelmontIzacc <jisagiizacc@gmail.com>
2017-05-30 15:28:42 -0500

    0.0002.2.4

--- resources/views/Welcome/contact.blade.php
+++ resources/views/Welcome/contact.blade.php
@@ -28,7 +28,8 @@
                     <div role="tabpanel" class="clearfix tab-pane active" id="tab-contact-1">
                         <div class="contacts-page-col-left">
                             <div class="contacts-page-col-left-in">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d20809.89660178715!2d-102.60623722765413!3d22.77633583635612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x86824e896598899f%3A0xa1491d46396ca8b4!2sPlaza+Bicentenario%2C+Blvd.+Adolfo+L%C3%B3pez+Mateos%2C+Zacatecas+Centro%2C+98000+Zacatecas%2C+Zac.!3m2!1d22.769448!2d-102.5719039!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUnidad+Profesional+Interdisciplinaria+de+Ingenier%C3%ADa+Campus+Zacatecas%2C+Cerro+del+Gato+Ejido+la+Escondida%2C+Col.+Ciudad+Administrativa%2C+Boulevard+El+Bote+S%2FN%2C+98160+Zacatecas%2C+Zac.!3m2!1d22.7839529!2d-102.61572989999999!5e0!3m2!1ses!2smx!4v1496174150865" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe><!--.contacts-page-col-left-in-->
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d1576.23089110793!2d-102.6161092753822!3d22.78330025591424!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUnidad+Profesional+Interdisciplinaria+de+Ingenier%C3%ADa+Campus+Zacatecas!5e1!3m2!1ses!2smx!4v1496070098317" width="978" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div><!--.contacts-page-col-left-in-->
                         </div><!--.contacts-page-col-left-->
                         <div class="contacts-page-col-right">
                             <section class="contacts-page-section">
@@ -36,29 +37,7 @@
                                 <a href="https://www.facebook.com/verodelarosa.medicaupiiz" class="contact">
                                     <i class="font-icon font-icon-facebook"></i>
                                 </a>
-                                <!--<a href="#" class="contact">
-                                    <i class="font-icon font-icon-linkedin"></i>
-                                </a>
-                                <a href="#" class="contact">
-                                    <i class="font-icon font-icon-google-plus"></i>
-                                </a>
-                                <a href="#" class="contact">
-                                    <i class="font-icon font-icon-instagram"></i>
-                                </a>-->
                             </section>
-                            <!--<form class="contacts-page-section">
-                                <header class="box-typical-header-sm">Send Message</header>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="E-Mail">
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Subject">
-                                </div>
-                                <div class="form-group">
-                                    <textarea rows="4" class="form-control" placeholder="Message"></textarea>
-                                </div>
-                                
-                            </form>-->
                             <section class="contacts-page-section">
                                 <header class="box-typical-header-sm">Correo y teléfonos:</header>
                                 <p class="contact-other"><i class="font-icon font-icon-mail"></i>johndoe@gmail.com</p>
--- resources/views/Welcome/tutorials.blade.php
+++ resources/views/Welcome/tutorials.blade.php
@@ -15,8 +15,22 @@ Servicio Medico Upiiz
 @include('alerts.sessionError')
 
 <div class="box-typical box-typical-padding documentation">
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=578903988956921";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-video" data-href="https://www.facebook.com/702542226521320/videos/887977814644426/" data-width="500" data-show-text="false"><blockquote cite="https://www.facebook.com/702542226521320/videos/887977814644426/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/702542226521320/videos/887977814644426/"></a><p>Evolution of Johnny depp
+
+Credit to Ida Engel</p>Posted by <a href="https://www.facebook.com/ZA-Photography-702542226521320/">ZA  Photography</a> on domingo, 17 de abril de 2016</blockquote></div>
     
 </div><!--.box-typical-->
+
 
 @stop
 
