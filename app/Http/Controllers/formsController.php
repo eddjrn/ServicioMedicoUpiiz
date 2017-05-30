a3c0515ad898bb9ab6f2855ecd274fa9f44fb078
eddjrn <eddjrn@gmail.com>
2017-05-30 16:12:52 -0500

    0.0002.2.5

--- app/Http/Controllers/formsController.php
+++ app/Http/Controllers/formsController.php
@@ -14,6 +14,12 @@ class formsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+     
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+     
     public function index()
     {
         $index = 7;
--- resources/views/Welcome/forms.blade.php
+++ resources/views/Welcome/forms.blade.php
@@ -15,7 +15,44 @@ Generación de formularios
 @stop
 
 @section('content')
-Hola
+<section class="tabs-section">
+    <div class="tabs-section-nav tabs-section-nav-icons">
+        <div class="tbl">
+            <ul class="nav" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#tabs-1-tab-1" role="tab" data-toggle="tab">
+                        <span class="nav-link-in">
+                            <i class="font-icon font-icon-doc"></i>
+                            Carta responsiva
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#tabs-1-tab-2" role="tab" data-toggle="tab">
+                        <span class="nav-link-in">
+                            <span class="font-icon font-icon-build"></span>
+                            Formato IMSS
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#tabs-1-tab-3" role="tab" data-toggle="tab">
+                        <span class="nav-link-in">
+                            <i class="font-icon font-icon-contacts"></i>
+                            Historia clinica
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div><!--.tabs-section-nav-->
+
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane fade in active" id="tabs-1-tab-1">Tab 1</div><!--.tab-pane-->
+        <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-2">Tab 2</div><!--.tab-pane-->
+        <div role="tabpanel" class="tab-pane fade" id="tabs-1-tab-3">Tab 3</div><!--.tab-pane-->
+    </div><!--.tab-content-->
+</section><!--.tabs-section-->
 @stop
 
 @section('scripts')
