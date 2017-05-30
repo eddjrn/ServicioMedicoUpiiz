a195605f0dcb026a47d00117a4a4ad5757d030fe
BelmontIzacc <jisagiizacc@gmail.com>
2017-05-30 15:01:07 -0500

    0.0002.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,10 +19,15 @@ Servicio Medico Upiiz
     </header>
     <div class="text-block text-block-typical">
         <p>
-            Objetivo principal el que los alumnos de nuevo ingreso no batallen con el llendo de la documentación médica.
+          Nuestra principal labor es la prevención, educación y detección de enfermedades. Se brinda  orientación  sobre las diferentes áreas de la salud, buscando mejorar la capacidad de aprendizaje de la Comunidad Politécnica,  por lo cual  se genera un historial clínico de cada uno de las personas que asisten a Servicio Médico a través de:   
+            <p>• Medicina Preventiva</p>   
+            <p>• Orientación de Nutrición</p>   
+            <p>• Consulta Médica</p>
+          Dentro de los servicios que además proporciona el Área de Servicio médico es:     
+            <p>1. Realizar la afiliación al Instituto Mexicano del Seguro Social (IMSS), a todos los alumnos de la Unidad Académica que no cuenten con alguna Seguridad Social ya sea por parte de sus padres  o de  trabajo, de contar con este servicio se deberá entregar la carta responsiva, ya que es un requisito obligatorio para los alumnos tener Seguro Social  para realizar  salidas  ó prácticas escolares fuera de la Unidad Académica.</p>     
+            <p>2. Todos los alumnos del IPN que  estén inscritos  cuentan con un Seguro de Vida contratado por el IPN, el cual cuenta con una cobertura durante las 24 hrs. de los 365 días del año y en cualquier lugar de la Repblica Mexicana.</p>                     ​  
         </p>
-        <p>mediante instructivos  y para los demás alumnos contar con su expediente completo.</p>
-        
+        <p>Horario de atención: 8:00 – 10:30  y de 11:00 - 15:00hrs  Encargada: Médica Verónica de la Rosa Sánchez </p>
     </div>
 </div><!--.box-typical-->
 
@@ -37,7 +42,7 @@ Servicio Medico Upiiz
 
                 <div class="tab-content widget-tabs-content">
 
-                    <div class="tab-pane" id="w-1-tab-0" role="tabpanel" aria-expanded="false">
+                    <div class="tab-pane" id="w-1-tab-0" role="tabpanel" aria-expanded="false" >
                         <center><iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d41623.14776222582!2d-102.59717057394697!3d22.765334713626213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x86824dfdee323e95%3A0x4b1a2d127500ff5c!2sUPIIZ-IPN%2C+Zacatecas!3m2!1d22.7839529!2d-102.61572989999999!4m5!1s0x86824931098d672b%3A0x81e350358c34bbd9!2sIMSS+Subdelegaci%C3%B3n+Zacatecas%2C+Av.+Solidaridad+2%2C+Militar%2C+98617+Guadalupe%2C+Zac.!3m2!1d22.7581779!2d-102.5222299!5e0!3m2!1ses!2smx!4v1495584465921" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></center>
                     </div>
 
@@ -66,19 +71,19 @@ Servicio Medico Upiiz
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="tab" href="#w-1-tab-1" role="tab" aria-expanded="false">
                                 <i class="font-icon font-icon-pin"></i>
-                                Clinica 1
+                                Clinica 1 (Zacatecas)
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="tab" href="#w-1-tab-2" role="tab" aria-expanded="false">
                                 <i class="font-icon font-icon-pin"></i>
-                                Clinica 4
+                                Clinica 4 (Guadalupe)
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link active" data-toggle="tab" href="#w-1-tab-3" role="tab" aria-expanded="false">
                                 <i class="font-icon font-icon-pin"></i>
-                                Clinica 3
+                                Clinica 3 (Guadalupe)
                             </a>
                         </li>
                     </ul>
