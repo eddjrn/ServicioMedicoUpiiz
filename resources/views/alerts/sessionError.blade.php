b68c3ab7a9e414fcadcb5a83209a24df68925bc3
eddjrn <eddjrn@gmail.com>
2017-06-03 17:36:03 -0500

    Delete sessionError.blade.php

--- resources/views/alerts/sessionError.blade.php
+++
@@ -1,8 +0,0 @@
-@if(Session::has('message-error'))
-    <div class="alert alert-danger alert-fill alert-close alert-dismissible fade in" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        {{Session::get('message-error')}}
-    </div>
-@endif
