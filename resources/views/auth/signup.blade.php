974ae2435d43c2bc61b23814c7e3154a88b62768
eddjrn <eddjrn@gmail.com>
2017-06-03 14:42:56 -0500

    0.0003.2.1

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -50,31 +50,6 @@
                     <span aria-hidden="true">&times;</span>
                 </button></a>
             {!!Form::close()!!}
-            
-            <!--<form class="sign-box">
-                <div class="sign-avatar no-photo">&plus;</div>
-                <header class="sign-title">UPIIZ</header>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="E-Mail"/>
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Boleta"/>
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Repetir boleta"/>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Contraseña"/>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Repetir Contraseña"/>
-                </div>
-                <button type="submit" class="btn btn-rounded btn-success sign-up">Registrar</button>
-                <p class="sign-note">¿Tienes una cuenta? <a href="sign-in.html">Iniciar sesion</a></p>
-                <!--<button type="button" class="close">
-                    <span aria-hidden="true">&times;</span>
-                </button>-->
-            </form>-->
         </div>
     </div>
 </div><!--.page-center-->
