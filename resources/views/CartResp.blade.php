f94c83beded3e39f23a0fef6bb5ce0ada9ef5af0
AleBelmont <rebelde.alejandra@hotmail.com>
2017-06-19 08:56:05 -0500

    0.001.2

---
+++ resources/views/CartResp.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+</head>
+<body>
+    <h1 align="Center">CARTA RESPONSIVA</h1>
+    </br>
+    <h3 align="Center">Zacatecas, Zacatecas a ______ de __________________ del ___________.</h3>
+    </br>
+    <h2>A quien corresponda:</h2>
+    <h3 align="justify">Por este medio me permito hacer constar que por así convenir, a mis intereses, y después de que las Autoridades de la Unidad Profesional Interdisciplinaria de Ingeniería Campus Zacatecas (UPIIZ), me informaron de la necesidad de llevar a cabo los trámites de Inscripción al Seguro Facultativo del Instituto Mexicano del Seguro Social, es mi voluntad  no realizar los trámites correspondientes, por tener una cobertura médica por parte de mis padres y/o trabajo, al mismo tiempo  que deslindo de toda responsabilidad presente o futura a esta Unidad Académica, que derive de  lo anterior.</h3>
+    <h2>ATENTAMENTE</h2>
+    <h3>NOMBRE DEL ALUMNO:______________________________________________</h3>
+    <h3>INSTITUCION QUE PRESTA SERVICIO MÉDICO:________________________</h3>
+    <h3>NUMERO DE SEGURIDAD SOCIAL (NSS):_______________________________</h3>
+    <h3>UNIDAD MÉDICO FAMILLIAR (UMF):___________ LUGAR:_______________</h3>
+    <h3>PROGRAMA ACADÉMICO:____________________________________________</h3>
+    <h3>No. DE BOLETA O PZ PROVISIONAL:___________________________________</h3>
+    <h3>GRUPO:______________________________________________________________</h3>
+    <h3>Correo Electrónico:___________________________ tel:_______________________</h3>
+    <h3>FIRMA DEL ALUMNO:________________________________________________</h3>
+    <h3>NOTA:</h3>
+    <h4 align="justify">ANEXA UNA COPIA DE TU CARTILLA NACIONAL DE SALUD O CARNET DE CITAS DONDE PUEDAS COMPROBAR QUE RECIBES ATENCIÓN MÉDICA POR TUS PADRES.</br>SI DESCONOCES ALGUNA INFORMACIÓN PREGUNTA AL ENTREGAR EL FORMATO EN SERVICIO MÉDICO DE LA UPIIZ</h4>
+</body>
+</html>
