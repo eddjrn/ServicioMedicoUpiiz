4d1d8a147f0a3eff5c41054d052b205833e7d63f
eddjrn <eddjrn@gmail.com>
2017-06-28 22:38:52 -0500

    0.0007.5.4

--- database/seeds/medicalRecordTableSeeder.php
+++ database/seeds/medicalRecordTableSeeder.php
@@ -24,44 +24,57 @@ class medicalRecordTableSeeder extends Seeder
             'fracturas' => 'hombro,brazo',
 
             'presionAlta' => '1', 
-			'diabetes' => '2',
-			'artritis' => '3',
-			'asma'  => '4',
-			'cancer' => '5',
-			'epilepsias' => '1',
-			'enfCorazon' => '2',
-			'sobrePeso' => '3',
-			'enfTiroides' => '4',
-			'bipolaridad' => '5',
-			'esquizofrenia' => '1',
-			'depresion' => '2',
+            'diabetes' => '2',
+            'artritis' => '3',
+            'asma'  => '4',
+            'cancer' => '5',
+            'epilepsias' => '1',
+            'enfCorazon' => '2',
+            'sobrePeso' => '3',
+            'enfTiroides' => '4',
+            'bipolaridad' => '5',
+            'esquizofrenia' => '1',
+            'depresion' => '2',
 
         ]);
 
-         DB::table('historiaMedica')->insert([
-            'usuario_id' => '3',
-
-            'alergias' => 'polvo,gatos',
+        $limit = 50;
+        $word = " abcdefghijklmnopqrstuvwxyzae i o u";
+        $capitalWord = " ABCDEFGHIJKLMNOPQRSTUVWXYZAEIOU";
+        
+        for($i = 0; $i <= $limit; $i++){
+            $allergy = "";
             
-            'fumar' => '0',
-            'alcohol' => '0',
-            'transfusiones' => '0',
-            'cirugias' => 'corazon',
-            'fracturas' => 'brazo',
+            for($j = 1; $j <= 4;$j++){
+                $allergy .= substr(str_shuffle($word), 0, rand (0 , 10)). ' ';
+            }
+            
+            DB::table('historiaMedica')->insert([
+                'usuario_id' => $i+3,
 
-            'presionAlta' => '1', 
-			'diabetes' => '2',
-			'artritis' => '3',
-			'asma'  => '4',
-			'cancer' => '5',
-			'epilepsias' => '1',
-			'enfCorazon' => '2',
-			'sobrePeso' => '3',
-			'enfTiroides' => '4',
-			'bipolaridad' => '5',
-			'esquizofrenia' => '1',
-			'depresion' => '2',
+                'alergias' => $allergy,
+                
+                'fumar' => rand(0 , 1),
+                'alcohol' => rand(0 , 1),
+                'transfusiones' => rand(0 , 1),
+                'cirugias' => substr(str_shuffle($word), 0, rand (0 , 10)),
+                'fracturas' => substr(str_shuffle($word), 0, rand (0 , 10)),
 
-        ]);
+                'presionAlta' => rand(1 , 5).rand(1 , 5), 
+                'diabetes' => rand(1 , 5).rand(1 , 5),
+                'artritis' => rand(1 , 5).rand(1 , 5),
+                'asma'  => rand(1 , 5).rand(1 , 5),
+                'cancer' => rand(1 , 5).rand(1 , 5),
+                'epilepsias' => rand(1 , 5).rand(1 , 5),
+                'enfCorazon' => rand(1 , 5).rand(1 , 5),
+                'sobrePeso' => rand(1 , 5).rand(1 , 5),
+                'enfTiroides' => rand(1 , 5).rand(1 , 5),
+                'bipolaridad' => rand(1 , 5).rand(1 , 5),
+                'esquizofrenia' => rand(1 , 5).rand(1 , 5),
+                'depresion' => rand(1 , 5).rand(1 , 5),
+
+            ]);
+        }
+         
     }
 }
