65fab0b8c16aec8f831c39aec5f64fd2cf6e8d52
BelmontIzacc <jisagiizacc@gmail.com>
2017-07-27 01:05:45 -0500

    0.0009.2.1

--- resources/views/Welcome/credits.blade.php
+++ resources/views/Welcome/credits.blade.php
@@ -15,16 +15,44 @@ Creditos
 @stop
 
 @section('content')
+
 <div class="row">
-	<div class="col-md-4"></div>
-	<div class="col-md-4">
-		<div class="card-grid-col">
+<div class="col-md-1"></div>	
+	<div class="col-md-9">
+
+<div class="container-fluid">
+			<div class="cards-grid" ><div class="column size-1of2"><div class="card-grid-col">
+					<article class="card-typical box-typical box-typical-padding documentation">
+						 <header class="documentation-header">
+        <h2>Objetivo</h2>
+        <p class="lead color-blue-grey"> </p>
+    </header>
+    <div class="text-block text-block-typical">
+        <p>
+          Nuestra principal labor es la prevención, educación y detección de enfermedades. Se brinda  orientación  sobre las diferentes áreas de la salud, buscando mejorar la capacidad de aprendizaje de la Comunidad Politécnica,  por lo cual  se genera un historial clínico de cada uno de las personas que asisten a Servicio Médico a través de: </br>  
+            </br><p>• Medicina Preventiva</p>   
+            <p>• Orientación de Nutrición</p>   
+            <p>• Consulta Médica</p>
+          </br>Dentro de los servicios que además proporciona el Área de Servicio médico es:     
+            </br></br><p>1. Realizar la afiliación al Instituto Mexicano del Seguro Social (IMSS), a todos los alumnos de la Unidad Académica que no cuenten con alguna Seguridad Social ya sea por parte de sus padres  o de  trabajo, de contar con este servicio se deberá entregar la carta responsiva, ya que es un requisito obligatorio para los alumnos tener Seguro Social  para realizar  salidas  ó prácticas escolares fuera de la Unidad Académica.</p>     
+            </br><p>2. Todos los alumnos del IPN que  estén inscritos  cuentan con un Seguro de Vida contratado por el IPN, el cual cuenta con una cobertura durante las 24 hrs. de los 365 días del año y en cualquier lugar de la Repblica Mexicana.</p>                     ​  
+        </p>
+        <p>Horario de atención: 7:00 - 17:00hrs </p>
+        
+    </div>
+					</article><!--.card-typical-->
+				</div><div class="card-grid-col">
+					
+				</div><div class="card-grid-col">
+					
+				</div></div><div class="column size-1of2"><div class="card-grid-col">
 					<article class="card-typical">
 						<div class="card-typical-section">
 							<div class="user-card-row">
 								<div class="tbl-row">
 									<div class="tbl-cell">
 										<p class="user-card-row-name">Elaboración</p>
+
 									</div>
 								</div>
 							</div>
@@ -32,10 +60,9 @@ Creditos
 						<div class="card-typical-section card-typical-content">
 							<header class="title"></header>
 							<p>Titulo : Servicio Medico Upiiz</p>
-							</br><p>Direccion : Belmont</p>
-							<p>Produccion : Norman</p>
-							<p>Diseño : Beltran</p>
-
+							</br><p>Direccion : Isaul Ibarra Belmonte</p>
+							<p>Produccion : Eduardo Javier reyes norman</p>
+							<p>Diseño : Olga Alejandra Beltran Silva</p>
 						</div>
 						<div class="card-typical-section">
 							<div class="card-typical-linked">
@@ -44,10 +71,42 @@ Creditos
 							</div>
 						</div>
 					</article><!--.card-typical-->
-				</div>
-	</div>
+				</div><div class="card-grid-col">
+					
+				</div><div class="card-grid-col">
+					
+				</div></div></div><!--.card-grid-->
+			<div class="clear"></div>
+
+		</div>
+		</div>
+		
+<style>
+
+footer {
+            position: relative;
+            /* Altura total del footer en px con valor negativo */
+            margin-top: -50px;
+            /* Altura del footer en px. Se han restado los 5px del margen
+               superior mas los 5px del margen inferior
+            */
+            height: 40px;
+            padding:5px 0px;
+            clear: both;
+            text-align: right;
+            
+        }
+
+</style>
+
+<footer>
+<div id="piedepagina">
+<p> v1.0 </p>
 </div>
+</footer>
+
 @stop
 
 @section('scripts')
 @stop
+
