217866db6a7ac02fabefb45508dc593aac1dcdbe
BelmontIzacc <jisagiizacc@gmail.com>
2017-07-29 15:00:27 -0500

    0.0009.6

--- resources/views/back/back.blade.php
+++ resources/views/back/back.blade.php
@@ -1,6 +1,136 @@
 <body background="https://brainstomping.files.wordpress.com/2017/07/castlevania-netflix.png">
 
 
+<style>
+    
+    html, body{
+    height: 97%; / * hacer que la página llene toda la altura * /
+}
+* {
+    margin: 0;  / * márgenes podrían meter la pata * /
+}
+font{
+    color: white; 
+    size: 12;
+}
+#principal{
+    height: 100%;  / * empuja el pie hacia abajo * /
 
+}
+#pie{
+    height: 110px;  / * establece la altura del pie de página * /
+    margin-top: -50px;  / * estas cifras tienen que ser opostas * /
+    text-align: left;
+}
+#cuerpo{
+    text-align: left;
+    font-size:1.3em;
+}
 
+#izq{
+    width: 50%;
+    /*border:2px solid;*/
+    height: 85vh;
+    float: left;
+    margin-top: 100px;
+}
+
+#der{
+    
+    width: 12%;
+    height: 40vh;
+    /*border:2px solid;*/
+    margin-top: 190px;
+    float: left;
+}
+
+#centro{
+    
+    width: 10%;
+    height: 20vh;
+    /*border:2px solid;*/
+    margin-top: 300px;
+    float: left;
+}
+
+#centroIz{
+    
+    width: 10%;
+    height: 20vh;
+    /*border:2px solid;*/
+    margin-top: 300px;
+    float: left;
+}
+
+</style>
+
+<div id="principal"> 
+            
+                
+                <div id="cuerpo">
+                    
+                    <div id="izq">
+                        <table border="0">
+                    <tr align="center">
+                        <td> <font color="#057303"> Puedes correr muy lejos</font></td>  
+                    </tr>
+                    <tr align="center">
+                        <td> <font color="#057303"> Y borrar tus huellas</font></td>
+                    </tr>
+                    <tr align="center">
+                        <td> <font color="#057303"> Pero, ¿Has escapado realmente? ¿Puedes llegar a escapar algun dia?</font></td>
+                    </tr>
+                    <tr align="center">
+                        <td> <font color="#057303"> O quizas es verdad que no posees ni la voluntad ni la astucia</font></td>
+                    </tr>
+                     <tr align="center">
+                        <td> <font color="#057303"> Para escapar al destino.</font></td>
+                    </tr>
+                     <tr align="center">
+                        <td> <font color="#057303"> Pero el mundo no es pequeño.</font></td>
+                    </tr>
+                     <tr align="center">
+                        <td> <font color="#057303"> Tu lo eres y el destino te persigue, donde quiera que te encuentres.</font></td>
+                    </tr>
+                    </table>
+                    </div>
+
+                    <div id="der">
+                        <table border="0">
+                            <tr align="center">
+                            </tr>
+
+                    <tr align="center">  
+                        <td> <font color="black">Izacc Belmont</font></td>
+                    </tr>
+                        </table>
+                    </div>                    
+                </div>
+            </table>
+        </div>
+    </div>
+
+<style>
+
+footer {
+            position: relative;
+            /* Altura total del footer en px con valor negativo */
+            margin-top: -50px;
+            /* Altura del footer en px. Se han restado los 5px del margen
+               superior mas los 5px del margen inferior
+            */
+            height: 30px;
+            padding:5px 0px;
+            clear: both;
+            text-align: right;
+            
+        }
+
+</style>
+
+<footer>
+<div id="piedepagina">
+<p> Isaul Ibarra Belmonte v1.0 </p>
+</div>
+</footer>
 </body>
